546d74bbb7925236ffda7dece639798e2ccc53be
Disintegrator-deathcore <dychko2004@gmail.com>
2024-02-19 15:10:36 +0500

    Обновлен внешний вид, добавлена кнопка для показа информации

---
+++ Build.bat
@@ -0,0 +1,3 @@
+pyinstaller -y -D --windowed --add-data "C:\Users\Disintero\AppData\Local\Programs\Python\Python311\Lib\site-packages\customtkinter;customtkinter" ./GUI.py -n TeleMove -i Logo.ico
+echo Build Comlite
+pause
--- GUI.py
+++ GUI.py
@@ -1,23 +1,14 @@
-from customtkinter import *
+from customtkinter import CTk
+from customtkinter import CTkButton, CTkCanvas, CTkLabel, CTkCheckBox, CTkFrame
+from customtkinter import set_default_color_theme, BOTH
 from customtkinter import filedialog as fd
-import matplotlib.pyplot as plt
-import ctypes
+# import ctypes
 import sqlite3
 
 
 font_for_btn = ("Comic Sans MS", 30)
-font_for_lbl = ("Comic Sans MS", 35)
+font_for_lbl = ("Comic Sans MS", 40)
 
-
-
-try:
-    conn = sqlite3.connect("data/TeleMove.db")
-    cur = conn.cursor()
-    
-    cur.execute("DELETE FROM path WHERE pathid=1")
-    conn.commit()
-except:
-    pass
 
 set_default_color_theme("green")
 class MainApplication(CTk):
@@ -25,7 +16,8 @@ class MainApplication(CTk):
         super().__init__()
         
         self.title("TeleMove")
-        self.geometry("600x400")
+        self.iconbitmap("Logo.ico")
+        self.geometry("600x450")
         self.resizable(width=False, height=False)
         self.conn = sqlite3.connect("data/TeleMove.db")
         self.cur = self.conn.cursor()
@@ -53,7 +45,7 @@ class MainApplication(CTk):
     def show_page(self, title):
         page = self.pages.get(title)
         if title == "Функции":
-            self.geometry("1000x1000")
+            self.geometry("780x620")
         
         if page:
             page.place(x=0, y=0, relwidth=1, relheight=1)
@@ -69,20 +61,40 @@ class HomePage(CTkFrame):
         
         self.directory = ""
         
-        self.greeting_lbl = CTkLabel(self, text="Здравствуйте", font=("Comic Sans MS", 25))
-        self.greeting_lbl.grid(row=0, column=0, padx=20, pady=20)
+        self.greeting_lbl = CTkLabel(self, text="Здравствуйте", font=font_for_lbl)
+        self.greeting_lbl.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
         
         self.btn_dir = CTkButton(self, text="Выбрать папку", font=font_for_btn, command=self.choose_directory, width=70, height=40)
-        self.btn_dir.grid(row=1, column=0, padx=70, pady=20)
+        self.btn_dir.grid(row=1, column=0, padx=120, pady=20, sticky="ew")
         
         self.btn_correct = CTkButton(self, text="Подтвердить", font=font_for_btn, command=self.click_on_correct, width=70, height=40)
-        self.btn_correct.grid(row=2, column=0, padx=70, pady=20)
+        self.btn_correct.grid(row=2, column=0, padx=120, pady=20, sticky="ew")
         
-        self.label_dir = CTkLabel(self, text="", font=font_for_lbl)
-        self.label_dir.grid(row=3, column=0, padx=20, pady=20)
+        self.insert_deep_dir_chkbx = CTkCheckBox(self, text="Включить подпапки", font=font_for_btn)
+        self.insert_deep_dir_chkbx.grid(row=3, column=0, padx=137, pady=20, sticky="ew")
+        
+        self.label_dir = CTkLabel(self, text="", font=("Comic Sans MS", 20))
+        self.label_dir.grid(row=4, column=0, padx=0, pady=0)
+        
+        self.info_btn = CTkButton(self, text="i", font=("Comic Sans MS", 25), command=self.call_info, width=40, height=10,
+                                  fg_color="#6A5ACD", hover_color="#3F32A4")
+        self.info_btn.grid(row=4, column=0, padx=10, pady=10, sticky="e")
+        
         
 
+    def call_info(self):
+        pass
+    
     def choose_directory(self):
+        try:
+            conn = sqlite3.connect("data/TeleMove.db")
+            cur = conn.cursor()
+            
+            cur.execute("DELETE FROM path WHERE pathid=1")
+            conn.commit()
+        except:
+            pass
+        
         self.directory = fd.askdirectory(title="Выбрать папку", initialdir="/")
         
         if self.directory:
@@ -107,39 +119,56 @@ class FuncPage(CTkFrame):
         
         self.app = app
         
-        self.canvas = CTkCanvas(self, width=300, height=300)
-        self.canvas.create_oval(50, 50, 250, 250, fill="red")
-        self.canvas.create_arc(50, 50, 250, 250, fill="green", start=0, extent=45)
-        self.canvas.grid(row=0, column=0, padx=20, pady=20)
+        self.show_diagramm_btn = CTkButton(self, text="Показать\nдиаграмму", font=("Comic Sans MS", 30), width=220, height=210,
+                                           command=self.show_diagram)
+        self.show_diagramm_btn.grid(row=0, column=0, padx=20, pady=20, rowspan=4)
         
         self.type_lbl = CTkLabel(self, text="Выберите тип сортировки", font=font_for_lbl)
-        self.type_lbl.grid(row=0, column=1, padx=20, pady=10, sticky="ew")
+        self.type_lbl.grid(row=0, column=1, padx=10, pady=10, sticky="nw")
         
         self.sort_by_name_btn = CTkButton(self, text="Сортировать по имени", font=font_for_btn,
                                        command=self.sort_by_name, width=25)
-        self.sort_by_name_btn.grid(row=1, column=1, padx=20, pady=20, sticky="ew")
+        self.sort_by_name_btn.grid(row=1, column=1, padx=20, pady=10, sticky="ew")
 
         self.sort_by_extension_btn = CTkButton(self, text="Сортировать по расширению", font=font_for_btn,
                                         command=self.sort_by_extension, width=25)
-        self.sort_by_extension_btn.grid(row=2, column=1, padx=20, pady=20, sticky="ew")
+        self.sort_by_extension_btn.grid(row=2, column=1, padx=20, pady=10, sticky="ew")
 
-        self.sort_by_size = CTkButton(self, text="Сортировать по размеру", font=font_for_btn,
+        self.sort_by_size_btn = CTkButton(self, text="Сортировать по размеру", font=font_for_btn,
                                    command=self.sort_by_size, width=25)
-        self.sort_by_size.grid(row=3, column=1, padx=20, pady=20, sticky="ew")
+        self.sort_by_size_btn.grid(row=3, column=1, padx=20, pady=10, sticky="ew")
+        
+        self.sort_by_meta_btn = CTkButton(self, text="Сортировать по метаданным", font=font_for_btn,
+                                      command=self.sort_by_meta, width=25)
+        self.sort_by_meta_btn.grid(row=4, column=1, padx=20, pady=10, sticky="ew")
+        
+        self.info_btn = CTkButton(self, text="i", font=("Comic Sans MS", 25), command=self.call_info, width=40, height=10,
+                                  fg_color="#6A5ACD", hover_color="#3F32A4")
+        self.info_btn.grid(row=5, column=1, columnspan=3, padx=10, pady=10, sticky="e")
+        
+    
+    def show_diagram(self):
+        from matplotlib.pyplot import bar, show
+        
+        ext = ["txt", "doc", "png", "jpg", "py"]
+        cnt = [120, 50, 23, 90, 55]
+        
+        bar(ext, cnt)
+        show()
+        pass
+    def call_info(self):
+        pass
         
     def sort_by_name(self):
-        self.app.cur.execute("SELECT * FROM path WHERE pathid=1")
-        path = self.app.cur.fetchone()[1] + "\\file.txt"
-        func_dll = ctypes.CDLL('C:/programming/pyCpp/TeleMove/func.dll')
-        func_dll.read_dir(path.encode("utf-8"))
+        pass
         
-    
     def sort_by_extension(self):
-        func_dll = ctypes.CDLL('./func.dll')
-        msg = func_dll.show_msg()
-        print(msg)
+        pass
     
     def sort_by_size(self):
+        pass
+    
+    def sort_by_meta(self):
         pass
 
 
